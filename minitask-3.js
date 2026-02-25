1e9448315f2a29b1e9949572565d38614683b6ec
Muhammad Naufal Fadhilah <117992655+nopalllfd@users.noreply.github.com>
2026-02-25 16:41:09 +0700

    refactor

--- minitask-3.js
+++ minitask-3.js
@@ -1,32 +1,21 @@
 function totalCharge(uang) {
   totalPurchase = 1000;
   totalCharge = uang - totalPurchase;
-  let totalPay = [];
+  console.log(`total belanja ${totalPurchase}, uang bayar ${uang}, total kembalian ${totalCharge}`);
   while (totalCharge > 0) {
-    if (totalCharge >= 50000) {
-      totalCharge = totalCharge - 50000;
-      console.log(50000);
-      totalPay.push(50000);
-    } else if (totalCharge >= 10000) {
-      totalCharge = totalCharge - 10000;
-      console.log(10000);
-      totalPay.push(10000);
-    } else if (totalCharge >= 5000) {
-      totalCharge = totalCharge - 5000;
-      console.log(5000);
-      totalPay.push(5000);
-    } else if (totalCharge >= 2000) {
-      totalCharge = totalCharge - 2000;
-      console.log(2000);
-      totalPay.push(2000);
-    } else if (totalCharge >= 1000) {
-      totalCharge = totalCharge - 1000;
-      console.log(1000);
-      totalPay.push(1000);
+    let noms = [50000, 10000, 5000, 2000, 1000];
+    let count = 0;
+    console.log('Detail kembalian');
+    for (let i = 0; i < noms.length; i++) {
+      if (totalCharge >= noms[i]) {
+        totalCharge - noms[i];
+        count++;
+      }
+      if (count > 0) {
+        console.log(`uang ${noms[i]} berjumlah ${count}`);
+      }
     }
+    break;
   }
-  console.log(totalPay);
-// output
 }
-
-totalCharge(10000);
+totalCharge(15000);
